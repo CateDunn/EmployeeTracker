6f5440b2e9d33fd6123644cfbacd571af2adddad
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-05-06 14:31:12 -0400

    adding connection to sql

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 const inquirer  = require('inquirer');
 const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    port: 3306,
+  
+    user: "root",
+  
+    password: "password",
+    database: "employee_trackerDB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    
+  });
