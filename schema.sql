ce2c36b04e9957e146093ab63c087e8e79591a10
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-05-11 16:49:54 -0400

    updating schema and seeds

--- schema.sql
+++ schema.sql
@@ -3,26 +3,26 @@ CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE departments (
-  id int AUTO_INCREMENT NOT NULL,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE roles (
-  id int AUTO_INCREMENT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL (10,4) NOT NULL,
-  department_id INT NOT NULL,
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE employees (
   id int AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id int NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,4) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE departments (
+  id int NOT NULL,
+  department_name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
